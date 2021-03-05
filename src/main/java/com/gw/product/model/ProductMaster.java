a2482bb13742ab45f8f4c5305a845bb428205acf
aviral092 <aviral.dixit@greatwits.com>
2021-03-05 22:25:54 +0530

    this is my products service

--- src/main/java/com/gw/product/model/ProductMaster.java
+++ src/main/java/com/gw/product/model/ProductMaster.java
@@ -10,9 +10,20 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 
 @Entity
 @Table(name="products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ProductMaster {
 	
 	@Id
@@ -29,53 +40,7 @@ public class ProductMaster {
 	
 
 	private String long_description;
-	private Date created_at;
-	
-	public ProductMaster() {	
-	}
-	
-	public ProductMaster(int product_id, String name, SubSubCategory subsubcategory, String short_description,
-			String long_description, Date created_at) {
-		super();
-		this.product_id = product_id;
-		this.name = name;
-		this.subsubcategory = subsubcategory;
-		this.short_description = short_description;
-		this.long_description = long_description;
-		this.created_at = created_at;
-	}
-
-	
-	  public int getProduct_id() { return product_id; }
-	  
-	  public void setProduct_id(int product_id) { this.product_id = product_id; }
-	  
-	  public String getName() { return name; }
-	  
-	  public void setName(String name) { this.name = name; }
-	  
-	  public SubSubCategory getSubsubcategory() { return subsubcategory; }
-	  
-	  public void setSubsubcategory(SubSubCategory subsubcategory) { this.subsubcategory = subsubcategory; }
-	  
-	  public String getShort_description()	{ return short_description; }
-	  
-	  public void setShort_description(String short_description) { this.short_description = short_description; }
-	  
-	  public String getLong_description() { return long_description; }
-	  
-	  public void setLong_description(String long_description) { this.long_description = long_description; }
-	  
-	  public Date getCreated_at() { return created_at; }
-	  
-	  public void setCreated_at(Date created_at) { this.created_at = created_at; }
-	  
-	 	@Override
-	public String toString() {
-		return "ProductMaster [product_id=" + product_id + ", name=" + name + ", subsubcategory=" + subsubcategory
-				+ ", short_description=" + short_description + ", long_description=" + long_description
-				+ ", created_at=" + created_at + "]";
-	}
-
+	private Date created_at; 
+	 	
 	
 }
--- src/main/java/com/gw/product/model/SubSubCategory.java
+++ src/main/java/com/gw/product/model/SubSubCategory.java
@@ -7,8 +7,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "sub_sub_category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class SubSubCategory {
 	
 	@Id
@@ -17,52 +28,5 @@ public class SubSubCategory {
 	private int sscId;
 	
 	private String sscName;
-//	
-//	@OneToMany(cascade=CascadeType.ALL)
-//	private List<ProductMaster> products;
-	
 
-	public SubSubCategory() {
-		
-	}
-	
-	public SubSubCategory(int sscId, String sscName //, List<ProductMaster> products
-			) {
-		super();
-		this.sscId = sscId;
-		this.sscName = sscName;
-//		this.products = products;
-	}
-
-	public int getSscId() {
-		return sscId;
-	}
-
-	public void setSscId(int sscId) {
-		this.sscId = sscId;
-	}
-
-	public String getSscName() {
-		return sscName;
-	}
-
-	public void setSscName(String sscName) {
-		this.sscName = sscName;
-	}
-
-	/*
-	 * public List<ProductMaster> getProducts() { return products; }
-	 * 
-	 * public void setProducts(List<ProductMaster> products) { this.products =
-	 * products; }
-	 */
-	@Override
-	public String toString() {
-		return "SubSubCategory [sscId=" + sscId + ", sscName=" + sscName + ", products=" 
-	//+ products 
-				+ "]";
-	}
-	
-	
-	
 }
